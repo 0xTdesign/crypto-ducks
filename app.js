c728b646e6aa90185e6973107ee92cbe8300b44e
jimbobdxb <tallisondxb@gmail.com>
2022-08-11 09:16:08 +0100

    update new website

---
+++ app.js
@@ -0,0 +1,32 @@
+
+let ducks = prompt("How many ducks you going to Mint ");
+
+let message
+
+if (ducks < 5){
+    message = "Little Duckling"
+}
+else if (ducks > 10) {
+    message = "Duck Whale"
+}
+
+else {
+    message = "Quack Quack "
+}
+
+// document.write(message);
+
+
+let mood = prompt (" How are you feeling 9 happy or sad")
+let moodMessage;
+
+if (mood === "happy") {
+    moodMessage = "i'm pleased your're happy!";
+} else if (mood === "sad") {
+    moodMessage = "Chin up lad, We'll get you there!";
+} else {
+    moodMessage = "... i dont understand how you feel."
+}
+
+document.write(message + "Your're feeling"+ mood + "," + moodMessage);
+
