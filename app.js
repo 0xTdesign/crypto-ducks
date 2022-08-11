ce8d26678a95dd0239df673e6de0a45634231051
jimbobdxb <tallisondxb@gmail.com>
2022-08-11 16:56:08 +0100

    For and while loops

--- app.js
+++ app.js
@@ -3,31 +3,50 @@
 
 
 
-function getDuck() {
-let output = ""
- let ducks = prompt("How many ducks you going to Mint ");
+// function getDuck() {
+// let output = ""
+//  let ducks = prompt("How many ducks you going to Mint ");
 
- let duckMessage
+//  let duckMessage
 
-if (ducks < 5){
-    duckMessage = "Little Duckling"
-}
-else if (ducks > 10) {
-    duckMessage = "Duck Whale"
-}
+// if (ducks < 5){
+//     duckMessage = "Little Duckling"
+// }
+// else if (ducks > 10) {
+//     duckMessage = "Duck Whale"
+// }
 
-else {
-    duckMessage = "Quack Quack "
-}
+// else {
+//     duckMessage = "Quack Quack "
+// }
 
-for ( let i = 0;i < ducks; i++) {
-    output = output + "<img src='duck-01.png' alt='duck' class='duck'/>"
-    console.log(i)
-}
+// for ( let i = 0;i < ducks; i++) {
+//     output = output + "<img src='duck-01.png' alt='duck' class='duck'/>" + i
+//     console.log(i)
+// }
 
-return document.write(duckMessage + output);
+// return document.write(duckMessage + output);
 
+//  }
+
+
+
+ function speedLimit () {
+    let speedlimit = 900
+
+    while (speedlimit > 60) {
+        speedlimit = prompt("What speed are you going?")
+       
+        if (speedlimit > 60){
+        alert("Thats to fast ")
+        speedlimit = speedlimit + "<img src='duck-01.png' alt='duck' class='duck'/>" + i
+        }
+    }
+    alert("Good driving")
  }
+
+
+
 
 
 // function getMood() {
